49c7237b13b59148759e1c95510aa8f4635b9469
Kevin Benelli <kevin.e.benelli@gmail.com>
2018-07-15 19:01:36 -0700

    styling changes and restructured the files

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,22 +1,30 @@
 import React, { Component } from "react";
-import "./Home.css";
+import "../styles/About.css";
 
 class About extends Component {
   render() {
     return (
       <div>
         <body>
-          <h1>Education</h1>
-          <p>UC Davis, Computer Science</p>
-          <p>
-            Santa Rosa Junior College, Computer Science/Mathematics (3.60 GPA)
-          </p>
+          <div className="sub-category">
+            <h1>Education</h1>
+            <p>UC Davis, Computer Science</p>
+            <p>
+              Santa Rosa Junior College, Computer Science/Mathematics (3.60 GPA)
+            </p>
+          </div>
 
-          <h1>Extracurricular </h1>
+          <div className="sub-category">
+            <h1>Extracurricular </h1>
+            <p>Computer Science Club: UC Davis</p>
+            <p>Computer Science Club: SRJC</p>
+          </div>
 
-          <h2>Hobbies</h2>
-          <p> Biking </p>
-          <p> Tenns </p>
+          <div className="sub-category">
+            <h2>Hobbies</h2>
+            <p> Biking </p>
+            <p> Tennis </p>
+          </div>
         </body>
       </div>
     );
