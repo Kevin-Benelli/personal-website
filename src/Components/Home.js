f586d3b84345af1690362fecdcc27c69afedfeff
Kevin Benelli <kevin.e.benelli@gmail.com>
2018-07-15 18:31:16 -0700

    added sub-title to home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,6 +10,11 @@ class Home extends Component {
         <div className="title-padding">
           <h1 className="name-title-text"> Kevin Benelli </h1>
           <img src={require("../pictures/resume-pic1.jpg")} />
+
+          <div className="sub-title ">
+            <p>Software Engineer Intern at Remot3.it</p>
+            <p>UC Davis, Computer Science</p>
+          </div>
         </div>
       </div>
     );
