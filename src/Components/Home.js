1558d62d57be93d73e51dfe8d2ebb62d5b117b07
Kevin Benelli <kevin.e.benelli@gmail.com>
2018-11-03 11:32:29 -0700

    fixed modules error

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-
+import ReactDOM from "react-dom";
+import Work from "./Work";
 import "../styles/Home.css";
+import { SocialIcon } from "react-social-icons";
 
 class Home extends Component {
   render() {
